628cf8b1c98508729315d3e98fd8bfbe83492636
brunoamaia <brunoamaia94@gmail.com>
2020-08-12 15:05:33 -0300

    Troca da Home

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 function openmenu() {
-    //$("#nav").css("right:", 0 + "px");
     event.preventDefault();
-    $('.nav').css({ 'right':'0px'});
-    $('.social').css({ 'right':'0px'});
+    document.getElementById("nav").style.right = "0px";
+    document.getElementById("social").style.right = "0px";
 }
 
 function closenmenu() {
-    //$("#nav").css("right:", 0 + "px");
     event.preventDefault();
-    $('.nav').css({ 'right':'-100%'});
-    $('.social').css({ 'right':'-100%'});
+    document.getElementById("nav").style.right = "-100%";
+    document.getElementById("social").style.right = "-100%";
 }
