76203d465ef43c8f31b0cdc7e62183be21447d46
brunoamaia <brunoamaia94@gmail.com>
2020-08-14 09:59:01 -0300

    Ajuste da página do Variadoflix e uma forçada no ajuste do tamanho do video

--- pages/projects/Ecoleta/video.js
+++ pages/projects/Ecoleta/video.js
@@ -1,18 +1,39 @@
-let windows = window.innerWidth;
 const video = document.querySelector(".video")
+let windowsInit = window.innerWidth;
 
-console.log(windows);
 
-if (windows < 400) {
-  video.innerHTML = `<iframe id="ytplayer" type="text/html" width="280" height="158" 
-  src="http://www.youtube.com/embed/Vy96B0IjAso"
-  frameborder="0" controls=0 >`
-} else if (windows < 600) {
-  video.innerHTML = `<iframe id="ytplayer" type="text/html" width="360" height="203" 
-  src="http://www.youtube.com/embed/Vy96B0IjAso"
-  frameborder="0" controls=0 >`
-} else {
-  video.innerHTML = `<iframe id="ytplayer" type="text/html" width="600" height="338" 
-  src="http://www.youtube.com/embed/Vy96B0IjAso"
-  frameborder="0" controls=0 >`
+function resize() {
+  if (windowsInit < 400) {
+    video.innerHTML = `<iframe id="ytplayer" type="text/html" width="280" height="158" 
+    src="http://www.youtube.com/embed/Vy96B0IjAso"
+    frameborder="0" controls=0 >`
+  } else if (windowsInit < 600) {
+    video.innerHTML = `<iframe id="ytplayer" type="text/html" width="360" height="203" 
+    src="http://www.youtube.com/embed/Vy96B0IjAso"
+    frameborder="0" controls=0 >`
+  } else {
+    video.innerHTML = `<iframe id="ytplayer" type="text/html" width="600" height="338" 
+    src="http://www.youtube.com/embed/Vy96B0IjAso"
+    frameborder="0" controls=0 >`
+  }
 }
+
+resize()
+
+function modification() {
+  let windowsNow = window.innerWidth;
+  if (windowsInit != windowsNow) {
+    resize()
+    windowsInit = windowsNow;
+  }
+
+  timer()
+}
+
+
+
+function timer() {
+  setTimeout(modification, 200)
+}
+
+window.onload = modification();
