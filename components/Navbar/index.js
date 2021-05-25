38ba5cbf0bcc4343982129811c7c25e430e20dbb
brunoamaia <brunoamaia94@gmail.com>
2021-05-25 17:46:49 -0300

    insert initial elements

---
+++ components/Navbar/index.js
@@ -0,0 +1,15 @@
+export function insertNavBar(currentPage, homeAddress) {
+
+}
+
+function openmenu() {
+  event.preventDefault();
+  document.getElementById("nav").style.right = "0px";
+  document.getElementById("social").style.right = "0px";
+}
+
+function closenmenu() {
+  event.preventDefault();
+  document.getElementById("nav").style.right = "-100%";
+  document.getElementById("social").style.right = "-100%";
+}
