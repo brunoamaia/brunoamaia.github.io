b7a94c6fe761a7a1df7c60aabfaca561c6d66929
brunoamaia <brunoamaia94@gmail.com>
2021-05-26 16:12:39 -0300

    create component footer

---
+++ utils/components/Footer.js
@@ -0,0 +1,39 @@
+/* How to use
+  import insertFooter from './Footer.js'
+
+  insertFooter('../')
+*/
+
+export default function insertFooter(homeAddress) {
+  // homeAddress = address to root page (../)
+
+  const footer = `
+    <div class="footer-container">
+      <div class="footer-columns">
+        <div class="ft-column">
+          <h3>Navegação</h3>
+          
+          <div>
+            <a href="${homeAddress}">Home</a> 
+            <a href="${homeAddress}pages/aboutme">Sobre</a> 
+            <a href="${homeAddress}pages/projects">Portifólfio</a>
+          </div>
+        </div>
+
+        <div class="ft-column">
+          <h3>Social</h3>
+
+          <div>
+            <a href="https://www.linkedin.com/in/brunoamaia94/">Linkedin</a>
+            <a href="https://github.com/brunoamaia">Github</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="footer-infos">
+        <p>Entre em contato para conhecer melhor e/ou informar problemas. </p>
+      </div>
+    </div>
+  `
+  document.querySelector(".footer-content").innerHTML = footer
+}
