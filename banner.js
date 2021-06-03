f8f6221ace734f6acd1fcf520cd09968240d0457
brunoamaia <brunoamaia94@gmail.com>
2021-06-03 17:27:42 -0300

    change data on homepage and apply style

--- banner.js
+++ banner.js
@@ -133,3 +133,16 @@ function clickNext() {
 }
 
 window.onload = timer();
+
+
+function changeStateMenu() {
+  let state = document.getElementById("toggleNavbar").checked;
+
+  if (state) {
+    document.querySelector(".links-pages").style.top = "0px"
+    document.querySelector("body").style.overflow = "hidden"
+  } else {
+    document.querySelector(".links-pages").style.top = "-100%"
+    document.querySelector("body").style.overflow = "visible"
+  }
+}
