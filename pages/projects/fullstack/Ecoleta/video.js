a7affe50065c6fa81d474e56e20e89e49f37b624
brunoamaia <brunoamaia94@gmail.com>
2021-06-14 12:19:33 -0300

    change url for more security

--- pages/projects/fullstack/Ecoleta/video.js
+++ pages/projects/fullstack/Ecoleta/video.js
@@ -3,17 +3,18 @@ let windowsInit = window.innerWidth;
 
 
 function resize() {
+  let route = 'https://www.youtube.com/embed/Vy96B0IjAso'
   if (windowsInit < 400) {
     video.innerHTML = `<iframe id="ytplayer" type="text/html" width="280" height="158" 
-    src="http://www.youtube.com/embed/Vy96B0IjAso"
+    src=${route}
     frameborder="0" controls=0 >`
   } else if (windowsInit < 600) {
     video.innerHTML = `<iframe id="ytplayer" type="text/html" width="360" height="203" 
-    src="http://www.youtube.com/embed/Vy96B0IjAso"
+    src=${route}
     frameborder="0" controls=0 >`
   } else {
     video.innerHTML = `<iframe id="ytplayer" type="text/html" width="600" height="338" 
-    src="http://www.youtube.com/embed/Vy96B0IjAso"
+    src=${route}
     frameborder="0" controls=0 >`
   }
 }
