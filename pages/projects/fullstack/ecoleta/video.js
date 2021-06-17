e2af7ebfc5df62ce16070075d49cda78d5849b27
brunoamaia <brunoamaia94@gmail.com>
2021-06-17 16:35:59 -0300

    insert style in projects: delivery ecoleta variadoflix

--- pages/projects/fullstack/ecoleta/video.js
+++ pages/projects/fullstack/ecoleta/video.js
@@ -1,24 +1,22 @@
-const video = document.querySelector(".video")
+const video = document.querySelector(".video-container")
 let windowsInit = window.innerWidth;
 
-
 function resize() {
+  let route = 'https://www.youtube.com/embed/Vy96B0IjAso'
   if (windowsInit < 400) {
     video.innerHTML = `<iframe id="ytplayer" type="text/html" width="280" height="158" 
-    src="http://www.youtube.com/embed/Vy96B0IjAso"
+    src=${route}
     frameborder="0" controls=0 >`
   } else if (windowsInit < 600) {
     video.innerHTML = `<iframe id="ytplayer" type="text/html" width="360" height="203" 
-    src="http://www.youtube.com/embed/Vy96B0IjAso"
+    src=${route}
     frameborder="0" controls=0 >`
   } else {
     video.innerHTML = `<iframe id="ytplayer" type="text/html" width="600" height="338" 
-    src="http://www.youtube.com/embed/Vy96B0IjAso"
+    src=${route}
     frameborder="0" controls=0 >`
   }
 }
-
-resize()
 
 function modification() {
   let windowsNow = window.innerWidth;
@@ -29,11 +27,9 @@ function modification() {
 
   timer()
 }
-
-
-
 function timer() {
   setTimeout(modification, 200)
 }
 
+resize()
 window.onload = modification();
